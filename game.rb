97a8584e8b6aa49c2d195fe5cf12cad1e522778e
Matías Bahamonde <mbahamondecastillo@gmail.com>
2022-11-22 17:19:02 -0300

    Update

--- game.rb
+++ game.rb
@@ -15,10 +15,13 @@ class Game
       # puts players
       board = Board.new
       board.print_board
+      players[0].play_turn(board)
+      board.print_board
     end
   end
 end
 
+# Board and grid to play
 class Board
   attr_reader :grid
 
@@ -26,21 +29,47 @@ class Board
     @grid = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
+  # Improve so it doesn't throw errors when printing "X" or "O"
   def print_board
-    puts ' %d | %d | %d' % @grid[0..2]
-    puts ' %d | %d | %d' % @grid[3..5]
-    puts ' %d | %d | %d' % @grid[6..8]
+    puts ' %d | %d | %d' % @grid[0..2].to_s
+    puts ' %d | %d | %d' % @grid[3..5].to_s
+    puts ' %d | %d | %d' % @grid[6..8].to_s
   end
 end
 
+# Methods to actuate over the board and play the game
 class Player
-  attr_accessor :marker, :turns
+  attr_accessor :marker, :turns, :plays
 
   def initialize(marker)
     @marker = marker
+    @plays = []
     @turns = 3
   end
 
+  def can_play?
+    turns.positive
+  end
+
+  def a_valid_play?(board, play)
+    board.grid[play - 1] == play
+  end
+
+  def register_play(board, play, marker)
+    plays << play
+    self.turns -= 1
+    board.grid[play.to_i - 1] = marker
+    puts plays
+  end
+
+  def play_turn(board)
+    until a_valid_play?(board, play = gets.chomp.to_i)
+      puts '%s Turn, Where would you like to place your marker?: ' % marker
+    end
+    register_play(board, play, marker)
+  end
+
+  # Is it possible to move methods as can_play, valid_play and register_play to self?
   class << self
     def players_picks_marker
       puts 'Player pick your marker: X | O'
